6dc87468944fb26ee24a178e19d001988f1f39f4
vvn dinesh <55079034+vvndinesh@users.noreply.github.com>
2019-09-30 12:54:49 +0530

    Create Newway.java

---
+++ Newway.java
@@ -0,0 +1,17 @@
+//**VADADA VENKATA NAGA DINESH**//
+//**Git hub :- vvndinesh**//
+
+class Newway
+{
+      public static void main(String[] args) 
+      {
+      	New.show();
+      }
+}
+class New
+{
+	public static void show()
+	{
+		System.out.println("java");
+	}
+}
